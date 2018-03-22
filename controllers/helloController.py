b1a7d07852b3351162063da5464c6e11ec1db290
Julien Heiduk <julien.heiduk@gmail.com>
2018-03-22 16:04:09 +0100

    Update helloController.py

--- controllers/helloController.py
+++ controllers/helloController.py
@@ -5,11 +5,11 @@ class HelloController(Resource):
     def get(self):
         return {"response" : "hello get"} 
     
-    def post(self):
-        return {"response" : "hello post"}
+    #def post(self):
+    #    return {"response" : "hello post"}
 
-    def put(self):
-        return {"response" : "hello put"}
+    #def put(self):
+    #    return {"response" : "hello put"}
 
-    def delete(self):
-        return {"response" : "hello delete"}
+    #def delete(self):
+    #    return {"response" : "hello delete"}
